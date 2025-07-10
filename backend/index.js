570034b3eb45c8f37d901866aa4205e759e1df92
Victoria Hernandez <vlhernan@meta.com>
2025-07-10 10:33:27 -0700

    changed cookie settings

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use(session({
     secret: 'capstone',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 } // 1-hour session
+    cookie: { secure: true, sameSite: 'none', httpOnly: true, maxAge: 1000 * 60 * 60 }
 }))
 
 app.use(authRoutes);
