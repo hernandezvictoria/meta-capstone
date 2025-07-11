8a7623dd7d963e09ead5c18d7b8f36547d3d242c
Victoria Hernandez <vlhernan@meta.com>
2025-07-11 16:09:21 -0700

    trsut proxy and samesite none

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { secure: process.env.NODE_ENV === "production",
+            sameSite: 'none',
              httpOnly: true,
              maxAge: 1000 * 60 * 60 }
 }))
