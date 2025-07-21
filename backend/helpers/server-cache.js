b48a96c2ed6a8c610ea0ce08fc33229a1373e93c
Victoria Hernandez <vlhernan@meta.com>
2025-07-21 16:21:40 -0700

    added docstrings

--- backend/helpers/server-cache.js
+++ backend/helpers/server-cache.js
@@ -4,24 +4,32 @@ const { PrismaClient } = require("../generated/prisma/index.js");
 const prisma = new PrismaClient();
 const { InteractionTypes, ActivityTypes } = require("../enums.js");
 
-const MAX_CACHE_SIZE = 50; // max number of products in cache
-const TTL = 1000 * 60 * 60 * 24; // time to live for each product in cache, 1 day for now
-const FLUSH_SIZE = 10; // number of products to flush from cache when cache is at capacity
+// ============ CONSTANTS ============
+const MAX_CACHE_SIZE = 70; // largest size dynamic cache can reach
+const MIN_CACHE_SIZE = 50;
 const PRIORITY_BOOST = 5; // how much to boost priority score by if product gets viewed multiple times from cache
-let currentUserId = null;
-const oneDayMilliseconds = 1000 * 60 * 60 * 24;
-const oneHourMilliseconds = 1000 * 60 * 60;
-const oneWeekMilliseconds = 1000 * 60 * 60 * 24 * 7;
+const ONE_DAY_MILLISECONDS = 1000 * 60 * 60 * 24;
+const ONE_HOUR_MILLISECONDS = 1000 * 60 * 60;
+const ONE_WEEK_MILLISECONDS = 1000 * 60 * 60 * 24 * 7;
+const CACHE_SIZE_LIMITOR = 1; // can be adjusted to less than 1 if app gets popular
 
+// ============ STATS ============
 let potentialAPICalls = 0;
 let cacheHits = 0;
 
+// ============ OTHER DATA ============
 let productQueue;
 let productImageCache;
-
-let numActiveUsers; // number of users active in the last hour
+let numActiveUsers = 0; // number of users active in the last hour
 let userToActivityScore; // map userID -> number of logins in past week normalized by max logins in that week by one user (between 0 and 1)
+let currentUserId = null;
+let dynamicCacheLifetime = 1000 * 60 * 60 * 24; // time to live for each product in cache, 1 day for now
+let dynamicFlushSize = 10; // number of products to flush from cache when cache is at capacity
+let dynamicCacheSize = MIN_CACHE_SIZE;
 
+/**
+ * Initializes cache and priority queue.
+ */
 const createQueueAndCache = () => {
   // priority queue for products, less interacted with products have lower priority (first to be flushed from cache)
   productQueue = new PriorityQueue((a, b) => {
@@ -40,17 +48,38 @@ const createQueueAndCache = () => {
   productImageCache = new Map(); // productId -> image url, timestamp
 };
 
-// sets num online users and user activities
-let maxScore = 1;
+/**
+ * Resizes cache based on number of active users in last hour.
+ */
+const resizeCache = () => {
+  dynamicCacheSize = MIN_CACHE_SIZE + (numActiveUsers * CACHE_SIZE_LIMITOR);
+  dynamicCacheSize = Math.min(dynamicCacheSize, MAX_CACHE_SIZE); // cap the cache size at MAX_CACHE_SIZE
+  dynamicCacheLifetime = ONE_DAY_MILLISECONDS / (dynamicCacheSize / MIN_CACHE_SIZE); // larger caches have shorter lifetimes
+  dynamicFlushSize = Math.floor(dynamicCacheSize / 5); // flush size is 1/5 of cache size
+
+  // if existing cache is larger than new cache size
+  if(productImageCache.size() > dynamicCacheSize){
+    const sizeDifference = productImageCache.size() - dynamicCacheSize;
+    const numFlushes = Math.ceil(sizeDifference / dynamicFlushSize); // use math.ceil to round up to the integer
+    for(i = 0; i < numFlushes; i++){
+      flushCache(); // flush until below max cache size
+    }
+  }
+}
+
+/**
+ * Sets number of active users in past hour (numActiveUsers) and activity scores of each user (userToActivityScore).
+ */
 const setUserActivity = async() => {
   numActiveUsers = 0;
   userToActivityScore = new Map();
+
   const allUserLogins = await prisma.userActivity.findMany({
     where: {activity_type : ActivityTypes.LOGIN}
   });
 
   const userLoginsInPastWeek = allUserLogins.filter((login) =>
-    (Date.now() - login.activity_time.getTime() <= oneWeekMilliseconds)
+    (Date.now() - login.activity_time.getTime() <= ONE_WEEK_MILLISECONDS)
   );
 
   const allUserLogouts = await prisma.userActivity.findMany({
@@ -60,11 +89,12 @@ const setUserActivity = async() => {
   });
 
   const userLogoutsInPastHour = allUserLogouts.filter((logout) =>
-    (Date.now() - logout.activity_time.getTime() <= oneHourMilliseconds)
+    (Date.now() - logout.activity_time.getTime() <= ONE_HOUR_MILLISECONDS)
   );
 
+  let maxScore = 1;
   for(const loginActivity of userLoginsInPastWeek){
-    if(Date.now() - loginActivity.activity_time.getTime() <= oneHourMilliseconds){ // if activity happened in last hour
+    if(Date.now() - loginActivity.activity_time.getTime() <= ONE_HOUR_MILLISECONDS){ // if activity happened in last hour
       // if the user hasn't logged out yet
       if(!userLogoutsInPastHour.some((logoutActivity) => {
           logoutActivity.user_id === loginActivity.user_id
@@ -90,16 +120,22 @@ const setUserActivity = async() => {
   for(const key of userToActivityScore.keys()){
     userToActivityScore.set(key, userToActivityScore.get(key) / maxScore) // now score is between 0 and 1
   }
+  resizeCache(); // update cache size after updating number of active users
 }
 
+/**
+ * Computes priority of a product based on how much interaction it gets.
+ * Higher priority scores are less likely to be flushed from the cache.
+ * @param {number} productId - ID of the product whose priority score to compute.
+ * @returns {number} - Computed priority score (float).
+ */
 // returns a number representing the priority of the product (based on how used it is)
 const computeInitialPriority = async (productId) => {
   await setUserActivity();
-  //higher click velocity -> higher priority score, less likely to be flushed from cache
   if (productImageCache.has(productId)) {
     const currentTime = Date.now();
     const productTime = productImageCache.get(productId).timestamp.getTime();
-    if (currentTime - productTime >= TTL) {
+    if (currentTime - productTime >= dynamicCacheLifetime) {
       return Number.MIN_SAFE_INTEGER; // if product data is stale, return lowest priority (first to be flushed)
     }
 
@@ -112,10 +148,8 @@ const computeInitialPriority = async (productId) => {
     if (totalProductClicks.length === 0) {
       return 0;
     }
-    const userClicksInLastDay = totalProductClicks.filter(
-      (interaction) =>
-        interaction.interaction_time.getTime() >=
-        currentTime - oneDayMilliseconds
+    const userClicksInLastDay = totalProductClicks.filter((interaction) =>
+      interaction.interaction_time.getTime() >= currentTime - ONE_DAY_MILLISECONDS
     );
 
     const interactionUsers = new Map(); // maps interaction type -> set of unqiue user ids
@@ -129,7 +163,7 @@ const computeInitialPriority = async (productId) => {
         if(userSet.has(userId)){
           isUserInSet = true;
         } else{
-          userSet.add(userId); // use set to only track distinct users
+          userSet.add(userId); // use a set to only track distinct users
         }
       }
       else{ // first time interaction is being added to the maps
@@ -149,6 +183,7 @@ const computeInitialPriority = async (productId) => {
       }
     }
 
+    // update interaction frequencies based on all clicks that happened in the last day
     for (const interaction of userClicksInLastDay) {
       switch (interaction.interaction_type) {
         case InteractionTypes.OPEN_MODAL:
@@ -175,21 +210,23 @@ const computeInitialPriority = async (productId) => {
       }
     }
 
+    // higher user interaction -> higher priority score -> less likely to be flushed from cache
     const openModalScore = interactionScores.get(InteractionTypes.OPEN_MODAL)
     const likeScore = interactionScores.get(InteractionTypes.LIKE) - interactionScores.get(InteractionTypes.REMOVE_LIKE);
     const saveScore = interactionScores.get(InteractionTypes.SAVE) - interactionScores.get(InteractionTypes.REMOVE_SAVE);
     const dislikeScore = interactionScores.get(InteractionTypes.DISLIKE) - interactionScores.get(InteractionTypes.REMOVE_DISLIKE);
 
     // opening modal carries most weight
-    // higher velocity -> higher priority score -> less likely to be flushed from cache
     return ( openModalScore + (likeScore + saveScore + dislikeScore) * 0.5 );
   } else {
     return Number.MIN_SAFE_INTEGER; // edge case if product not in cache, should never happen, but if it does, will get flushed first
   }
 };
 
+/**
+ * Flushes stale data from cache or dynamicFlushSize products from cache in order of priority (lowest to highest).
+ */
 const flushCache = () => {
-  // flush stale data from cache or FLUSH_SIZE products from cache in priority order
   let totalRemoved = 0;
   if (productImageCache.size > 0) {
     // remove all the products with stale data from cache
@@ -203,12 +240,12 @@ const flushCache = () => {
       }
     }
 
-    if (totalRemoved >= FLUSH_SIZE) {
+    if (totalRemoved >= dynamicFlushSize) {
       return; // if enough products have already been removed, return
     }
 
     // remove more items from cache if not enough products with stale data were removed
-    for (let i = 0; i < FLUSH_SIZE - totalRemoved; i++) {
+    for (let i = 0; i < dynamicFlushSize - totalRemoved; i++) {
       if (productImageCache.size === 0 || productQueue.isEmpty()) {
         return; // if all products are removed, return
       }
@@ -218,12 +255,16 @@ const flushCache = () => {
   }
 };
 
+/**
+ * Inserts product into the cache and priority queue.
+ * @param {number} productId - ID of the product to be inserted.
+ */
 const insertProduct = async (productId) => {
   if (!productQueue) {
     createQueueAndCache(); // if queue and cache are not created, create them
   }
 
-  if (productImageCache.size >= MAX_CACHE_SIZE) {
+  if (productImageCache.size >= dynamicCacheSize) {
     flushCache(); // if cache is at capacity, flush FLUSH_SIZE products from cache
   }
   productImageCache.set(productId, {
@@ -234,6 +275,10 @@ const insertProduct = async (productId) => {
   productQueue.enqueue({ productId, priority });
 };
 
+/**
+ * Replaces stale product data with updated product data in cache and queue.
+ * @param {number} productId - ID of product to be replaced.
+ */
 const replaceProduct = async (productId) => {
   productQueue.remove((p) => p.productId === productId);
   productImageCache.set(productId, {
@@ -244,7 +289,12 @@ const replaceProduct = async (productId) => {
   productQueue.enqueue({ productId, priority });
 };
 
-// returns the image url of the product, given the product ID
+/**
+ * Fetches the image URL of the product from the local cache, else from DB.
+ * @param {number} userId - ID of the user who is fetching the product.
+ * @param {number} productId - ID of the product whose image to fetch.
+ * @returns {string} - The product image's URL.
+ */
 const getProductImage = async (userId, productId) => {
   if (!currentUserId) { // create cache if it doesn't exist
       createQueueAndCache();
@@ -260,7 +310,7 @@ const getProductImage = async (userId, productId) => {
     }
     currentUserId = userId; // set the current user id
     if (productImageCache.has(productId)) {
-      if ( Date.now() - productImageCache.get(productId).timestamp.getTime() >= TTL ) {
+      if ( Date.now() - productImageCache.get(productId).timestamp.getTime() >= dynamicCacheLifetime ) {
         await replaceProduct(productId); // if product data is stale, replace it with new data
       } else {
         cacheHits++; // if product data is not stale, increment cache hits
