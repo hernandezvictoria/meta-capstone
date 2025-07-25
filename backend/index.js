ccb24d491df3649a80cbfe08754466f7a8b55a9e
Victoria Hernandez <vlhernan@meta.com>
2025-07-25 13:43:16 -0700

    changed formatting

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const session = require("express-session");
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("welcome to skinterest!");
+    res.send("welcome to skinterest!");
 });
 
 const authRoutes = require("./routes/auth");
@@ -18,30 +18,30 @@ const routineRoutes = require("./routes/routine");
 const { ValidationError } = require("./middleware/CustomErrors");
 
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    })
 );
 
 app.use(express.json());
 
 app.use(
-  session({
-    secret: "capstone",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      domain:
-        process.env.NODE_ENV === "production"
-          ? ".meta-capstone-backend.onrender.com"
-          : "localhost",
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60,
-    },
-  })
+    session({
+        secret: "capstone",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            domain:
+                process.env.NODE_ENV === "production"
+                    ? ".meta-capstone-backend.onrender.com"
+                    : "localhost",
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60,
+        },
+    })
 );
 
 app.set("trust proxy", 1);
@@ -53,12 +53,12 @@ app.use(cacheRoutes);
 app.use(routineRoutes);
 
 app.use((err, req, res, next) => {
-  if (err instanceof ValidationError) {
-    return res.status(err.statusCode).json({ error: err.message });
-  }
-  res.status(500).json({ error: "Internal Server Error" });
+    if (err instanceof ValidationError) {
+        return res.status(err.statusCode).json({ error: err.message });
+    }
+    res.status(500).json({ error: "Internal Server Error" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
