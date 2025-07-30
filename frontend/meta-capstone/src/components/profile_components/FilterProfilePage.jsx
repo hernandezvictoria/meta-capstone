145017737c2ceab680565d8dcf5b9a366fa01116
Victoria Hernandez <vlhernan@meta.com>
2025-07-30 14:04:57 -0700

    added styling details to profile

--- frontend/meta-capstone/src/components/profile_components/FilterProfilePage.jsx
+++ frontend/meta-capstone/src/components/profile_components/FilterProfilePage.jsx
@@ -53,7 +53,7 @@ function FilterProfilePage() {
                 <button
                     aria-label="show liked products"
                     id={ProfileFilters.LIKED}
-                    className="filter-button"
+                    className={`filter-button ${selectedFilter === ProfileFilters.LIKED ? "filter-button-selected" : ""}`}
                     onClick={onFilterClick}
                 >
                     loved
@@ -61,7 +61,7 @@ function FilterProfilePage() {
                 <button
                     aria-label="show saved products"
                     id={ProfileFilters.SAVED}
-                    className="filter-button"
+                    className={`filter-button ${selectedFilter === ProfileFilters.SAVED ? "filter-button-selected" : ""}`}
                     onClick={onFilterClick}
                 >
                     saved
@@ -69,7 +69,7 @@ function FilterProfilePage() {
                 <button
                     aria-label="show disliked products"
                     id={ProfileFilters.DISLIKED}
-                    className="filter-button"
+                    className={`filter-button ${selectedFilter === ProfileFilters.DISLIKED ? "filter-button-selected" : ""}`}
                     onClick={onFilterClick}
                 >
                     disliked
@@ -77,7 +77,7 @@ function FilterProfilePage() {
                 <button
                     aria-label="show user info"
                     id={ProfileFilters.USERINFO}
-                    className="filter-button"
+                    className={`filter-button ${selectedFilter === ProfileFilters.USERINFO ? "filter-button-selected" : ""}`}
                     onClick={onFilterClick}
                 >
                     view profile
